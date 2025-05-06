f6f5d6d748f959399511eef66eafe2a0242c0bbd
Sam <quocdoansam.me@gmail.com>
2025-05-07 00:33:53 +0700

    Add network configuration for Sepolia in Hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
